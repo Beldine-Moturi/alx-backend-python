5b11ba083b9c88cacbe2daca4915deb5c3750d84
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-08 00:54:57 +0300

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines a function that concatenates strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Cocatenates str1 and str2"""
+
+    return str1 + str2
