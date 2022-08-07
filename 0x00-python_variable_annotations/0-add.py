002dfa56eac4f6a0a991bdcbe2a9d96d8a573e0d
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-08 00:44:00 +0300

    fix pep8 missing whitespace

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Defines the types-annotated function add"""
 
-def add(a: float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """Returns sum of a and b as a float"""
 
     return a + b
