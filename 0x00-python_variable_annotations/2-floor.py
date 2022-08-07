fa7eb53c5fdab64feb219a859e6eb26c55fcda60
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-08 01:09:19 +0300

    task 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines a function floor that returns the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n"""
+
+    return int(n // 1)
