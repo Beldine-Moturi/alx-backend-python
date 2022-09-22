62904100a9d3c5df77782d953a8191e9d44bc457
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-22 23:54:12 +0300

    mock a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,7 +17,7 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org_name, mock_get_json):
         """Tests that GithubOrgClient.org returns the correct value"""
 
-        url = f"https://api.github.com/orgs/{org_name}"
+        url = "https://api.github.com/orgs/{}".format(org_name)
         spec = GithubOrgClient(org_name)
         spec.org()
         mock_get_json.assert_called_once_with(url)
